a92f052e7472b28415eaad66d6fe97369c0d28c4
Koyubin <yubindev1998@gmail.com>
2024-01-01 01:05:16 +0900

    add part 8

--- docs/.vitepress/sidebar/part/8.mjs
+++ docs/.vitepress/sidebar/part/8.mjs
@@ -1,6 +1,89 @@
+const chapter1 = {
+  text: "Chapter 1 디자인 로직 사용",
+  link: "/contents/part/8/chapter/1/using-design-logic",
+  items: [
+    {
+      text: "Section 1 유체 논리",
+      link: "/contents/part/8/chapter/1/section/1",
+    },
+    {
+      text: "Section 2 비즈니스 로직",
+      link: "/contents/part/8/chapter/1/section/2",
+    },
+  ],
+};
+
+const chapter2 = {
+  text: "Chapter 2 스타일의 진화",
+  link: "/contents/part/8/chapter/2/evolving-a-style",
+  items: [
+    {
+      text: "Section 1 디자인 무결성",
+      link: "/contents/part/8/chapter/2/section/1",
+    },
+  ],
+};
+
+const chapter3 = {
+  text: "Chapter 3 매력의 영향을 이해하기",
+  link: "/contents/part/8/chapter/3/appreciating-the-impact-of-attractiveness",
+  items: [
+    {
+      text: "Section 1 시선 추적 및 미적 반응",
+      link: "/contents/part/8/chapter/3/section/1",
+    },
+    {
+      text: "Section 2 “장” 반응",
+      link: "/contents/part/8/chapter/3/section/2",
+    },
+    {
+      text: "Section 3 정서적 반응과 의사결정",
+      link: "/contents/part/8/chapter/3/section/3",
+    },
+  ],
+};
+
+const chapter4 = {
+  text: "Chapter 4 시각 디자인",
+  items: [
+    {
+      text: "Section 1 시각적 평면 형성",
+      link: "/contents/part/8/chapter/4/section/1",
+    },
+    {
+      text: "Section 2 지각의 게슈탈트 원리를 사용하여",
+      link: "/contents/part/8/chapter/4/section/2",
+    },
+  ],
+};
+
+const chapter5 = {
+  text: "Chapter 5 시각적 구조",
+  link: "/contents/part/8/chapter/5/visual-structure",
+  items: [
+    {
+      text: "Section 1 시각적 구조의 기원 이해",
+      link: "/contents/part/8/chapter/5/section/1",
+    },
+    {
+      text: "Section 2 시각적 계층 구조 설정",
+      link: "/contents/part/8/chapter/5/section/2",
+    },
+    {
+      text: "Section 3 페이지 그리드 시스템 사용",
+      link: "/contents/part/8/chapter/5/section/3",
+    },
+    {
+      text: "Section 4 즐겨찾는 스타일 및 패턴 라이브러리",
+      link: "/contents/part/8/chapter/5/section/4",
+    },
+  ],
+};
+
 export const part8 = [
   {
     text: "Part 8 그래픽 디자인",
     link: "/contents/part/8/graphic-design",
+    items: [chapter1, chapter2, chapter3, chapter4, chapter5],
   },
 ];
